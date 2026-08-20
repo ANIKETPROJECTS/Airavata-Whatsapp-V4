626e527e1044ce0d6c343a0ef8a309944c0cb054
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-08-20 10:42:35 +0000

    s

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -18,7 +18,7 @@ module.exports = {
 
         // ── Session ───────────────────────────────────────────────────────────
         SESSION_SECRET: "07O2oC0/xUfVvayY/JZuxiSxV78OiXyJUq6Jwi01lGW5qBSDuDPNYriUwnh5lV5qd4Pu21/C5CeHTjS3vxdt/Q==",
-        WHATSAPP_CREDENTIALS_KEY: process.env.WHATSAPP_CREDENTIALS_KEY,
+        WHATSAPP_CREDENTIALS_KEY: "3c0b539c32c87de0ac022b3659329f1c4a7a5cec7582ce913baa2cd41187fb3f",
 
         // ── Meta / WhatsApp ───────────────────────────────────────────────────
         META_ACCESS_TOKEN: "EAAS0h8ZCJ7eIBR9DUEyTZChhRc43DyaRwCiZBzNa440O8K2OdfkpxuKnGZAKqXqdYwqfw1q7EUAW9ZBK2zpjYxXiMqsCgQZCodIa8SZAm0HsIcNLZCLAPZCYF9t3CjVQLu9u5HnBJbmLpDGrQkHTiCWPFRIZBr7ObfPepaPxsDLNrngjwAZBhFeUzVBdYvY3ZALM4aP1FwZDZD",
